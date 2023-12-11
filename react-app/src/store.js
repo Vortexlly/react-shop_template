fb38c7d4c233fc6609a9fa6fe8f9a6e793299f8c
YuraStruminskiy <ystruminskiy@gmail.com>
2023-12-11 13:36:08 +0200

    localStorage added

--- react-app/src/store.js
+++ react-app/src/store.js
@@ -1,10 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from './slice';
+import { loadState, saveState } from "./localStorage";
 
 const store = configureStore({
     reducer: {
         cart: cartReducer
-    }
+    },
+    preloadedState: loadState(),
 });
+
+store.subscribe(() => {
+    saveState(store.getState()); // Зберігаємо стан після кожного оновлення
+  });
 
 export default store;
